ba3691f5866c549d8575f950aed6de478a2c8fa6
LiviuSopon <liviusopon@gmail.com>
2020-12-17 22:31:25 +0200

    Some progress

--- shell.cpp
+++ shell.cpp
@@ -6,6 +6,9 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #include <readline/readline.h>
 #include <readline/history.h>
@@ -53,19 +56,30 @@ int main(int argc, char **argv)
             if(input[0] != '\0') // if input is not empty line
                 add_history(input);
         
-        auto args = cpplit(input, " ");
-        //cout<<args[1];
-
-        char* args_c[args.size()+1];
-        args_c[args.size()] = NULL;
-        for (int i = 0; i < args.size(); i++) {
-            args_c[i] = (char*)args[i].c_str();
-        }
-
-        printf("%s\n", args_c[1]);
-
-
         if(strcmp(input, "exit\0") == 0) break; // break execution only on "exit"
+
+        auto commands = cpplit(input, "|");
+        for (int i = 0; i < commands.size(); i++) {
+            auto args = cpplit(commands[i].c_str(), " ");
+            char* cmd = (char*)args[0].c_str(); // first argument for execvp
+            char* args_c[args.size()+1];
+            args_c[args.size()] = NULL;
+            for (int i = 0; i < args.size(); i++) {
+                args_c[i] = (char*)args[i].c_str(); // second argument for execvp
+            } // it ain't much, but it's honest work
+
+            int pid = fork();
+            if (pid < 0) {
+                cout<<"Yo, dawg, there's been an error.";
+                exit(-1);
+            }
+            else if (pid == 0) {
+                execvp(cmd, args_c);
+            }
+            else {
+                wait(NULL);
+                }
+            }
 
        // printf("Current line = %s\n", input);
         free(input); // tbh, I read it's good practice to do this
